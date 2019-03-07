cbc8772adbb4ded08c3c9a91b197c0224a7523ca
Alexander Tomokhov <alexoundos@gmail.com>
2019-03-07 19:16:26 +0300

    towards typeclass mock method

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Main where
 
 import Control.Monad.State
@@ -5,26 +7,43 @@ import Control.Monad.State
 
 -- | Screen image data (for example, it can be Picture from Gloss).
 -- For simplicity of the example it's just Float.
-type ScreenImage = Float
+type ScreenImage = Int
 
 -- | Camera shot image. Different types to distinguish more easily from
 -- ScreenImage.
-type ShotImage = Int
+type ShotImage = Float
 
 -- | Draw image on the screen.
-type DrawImage = ScreenImage -> IO ()
+type DrawImage m = ScreenImage -> m ()
 
 -- | Take a shot of the screen.
-type TakeShot = IO ShotImage
+type TakeShot m = m ShotImage
 
+-- | Операции отображения и съёмки в общем контексте. Полезно для симуляции IO
+-- при тестировании.
+class Monad m => MonadDrawShot m where
+  drawImage :: DrawImage m
+  takeShot :: TakeShot m
+
+
+instance MonadDrawShot IO where
+  drawImage = print
+  takeShot = read <$> getLine
+
+instance MonadDrawShot (State ScreenImage) where
+  drawImage = put
+  takeShot = fromIntegral <$> get
 
 initialScreenImage :: ScreenImage
-initialScreenImage = 0.0
+initialScreenImage = 0
 
 main :: IO ()
 main = do
   _ <- runStateT ioSimulationT initialScreenImage
   return ()
+
+test = do
+  runState (realFunctionUsingIO drawImage takeShot) initialScreenImage
 
 ioSimulationT :: StateT ScreenImage IO ()
 -- ioSimulationT = liftIO $ realFunctionUsingIO (\_ -> print 1.0) (return 5)
@@ -34,26 +53,24 @@ ioSimulationT = do
   liftIO $ realFunctionUsingIO (\_ -> print 1.0) (liftIO (return shot))
 
 -- | Draw image on screen simulation.
-simulateDraw :: ScreenImage -> StateT ScreenImage IO ()
+simulateDraw :: MonadDrawShot m => ScreenImage -> StateT ScreenImage m ()
 simulateDraw image = put image
 
 -- | Take shot of the screen simulation.
-simulateShot :: StateT ScreenImage IO ShotImage
-simulateShot = round <$> get
+simulateShot :: MonadDrawShot m => StateT ScreenImage m ShotImage
+simulateShot = fromIntegral <$> get
 
 -- | Produces new ScreenImage from ShotImage.
 calibrateImage :: ShotImage -> ScreenImage
-calibrateImage = (+) 1 . fromIntegral
+calibrateImage = (+) 1 . round
 
 -- | Function that uses functions with real devices. We cannot touch
 -- implementation of this function, that's the point.
-realFunctionUsingIO :: DrawImage -> TakeShot -> IO ()
-realFunctionUsingIO drawImage takeShot = do
-  shotImage1 <- takeShot
-  print shotImage1
-  drawImage (calibrateImage shotImage1)
-  shotImage2 <- takeShot
-  print shotImage2
-  drawImage (calibrateImage shotImage2)
-  shotImage3 <- takeShot
-  print shotImage3
+realFunctionUsingIO :: MonadDrawShot m => DrawImage m -> TakeShot m -> m ()
+realFunctionUsingIO drawImage' takeShot' = do
+  shotImage1 <- takeShot'
+  drawImage' (calibrateImage shotImage1)
+  shotImage2 <- takeShot'
+  drawImage' (calibrateImage shotImage2)
+  shotImage3 <- takeShot'
+  drawImage' (calibrateImage shotImage3)
